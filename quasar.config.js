725b0975844b60aa1eeef28ebab98ba0b1f71955
Cleiton Rieke <cleitonrieke@gmail.com>
2024-09-02 21:09:26 -0300

    novo teste com variaveis de ambiente

--- quasar.config.js
+++ quasar.config.js
@@ -48,6 +48,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      env: { URL_BACKEND: process.env.URL_BACKEND },
       target: {
         browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
         node: "node20",
